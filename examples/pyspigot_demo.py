1ced19014cb3815588ee137d33f44c987246bbf9
R7LRod <r7lrodney@outlook.com>
2025-10-25 06:31:11 +0000

    cleanup: remove deprecated example files and compiled artifacts

--- examples/pyspigot_demo.py
+++
@@ -1,6 +0,0 @@
-import sys, os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-from pyspigot import Bukkit
-print('PySpigot/Bukkit demo')
-print(Bukkit.getName())
